0457641ec303b7bd063b35fa0c0e3a289bdfe2b7
Jason Platt <donquixote@Jasons-MacBook-Pro.local>
2020-04-17 21:43:38 -0700

    more print statements

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -100,6 +100,7 @@ if __name__=="__main__":
                 writer.writerow(stats)
                 all_cpu.append(cpu)
                 all_time.append(times)
+                print(n, num_proc)
     np.savez_compressed('raw_data', cpu = all_cpu, times = all_time)
     print('Finished')
 
